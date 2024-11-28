5901f814db16822fbc24943e43ae8592e826d0a3
Yousef-Attar <yousefattar961@gmail.com>
2024-11-28 09:21:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    let saveEl = document.getElementById("save-el");
-    let countEl = document.getElementById("count-el");
-    let count = 0;
-    let sumLaps = 0;
-    let finishBtn = "The laps for today are: ";
+    let saveEl = document.getElementById("save-el")
+    let countEl = document.getElementById("count-el")
+    let count = 0
+    let sumLaps = 0
+    let finishBtn = "The laps for today are: "
 
     // Increment function
     function increment() {
-        count += 1;
-        countEl.textContent = count;
+        count += 1
+        countEl.textContent = count
     }
 
     function save() {
-        let countStr = count;
-        sumLaps += count;
+        let countStr = count
+        sumLaps += count
 
         if (count !== 0) {
-            saveEl.textContent = "Last finished laps are " + countStr;
+            saveEl.textContent = "Last finished laps are " + countStr
         } else {
-            saveEl.textContent = "No finished laps!!";
+            saveEl.textContent = "No finished laps!!"
         }
 
         if (count !== 0) {
             if (finishBtn === "The laps for today are: ") {
-                finishBtn += " " + count;
+                finishBtn += " " + count
             } else {
-                finishBtn += " - " + count;
+                finishBtn += " - " + count
             }
         }
 
-        countEl.textContent = 0;
-        count = 0;
+        countEl.textContent = 0
+        count = 0
     }
 
     function finish() {
         if (sumLaps === 0) {
-            saveEl.innerText = "You haven't finished any laps!";
+            saveEl.innerText = "You haven't finished any laps!"
         } else {
-            saveEl.innerText = finishBtn + " = " + sumLaps;
+            saveEl.innerText = finishBtn + " = " + sumLaps
         }
 
-        finishBtn = "The laps for today are: ";
-        count = 0;
-        sumLaps = 0;
+        finishBtn = "The laps for today are: "
+        count = 0
+        sumLaps = 0
     }
-    document.getElementById("increment-btn").addEventListener("click", increment);
-    document.getElementById("save-btn").addEventListener("click", save);
-    document.getElementById("finish-btn").addEventListener("click", finish);
-});
+    document.getElementById("increment-btn").addEventListener("click", increment())
+    document.getElementById("save-btn").addEventListener("click", save())
+    document.getElementById("finish-btn").addEventListener("click", finish())
+})
